253fbe84d79f9e985db1d3d6852816a3083a3747
Jeff Craley <jcraley@gmail.com>
2020-05-28 12:22:10 -0400

    new images

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,28 +1,46 @@
 import os
 import utils.output_tools as out
 import matplotlib
+import numpy as np
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
 
-def make_images(fns, preds, labels, viz_folder, prefix, suffix):
+def make_images(fns, preds, labels, viz_folder, prefix, suffix, threshold=None):
     for fn, pred, label in zip(fns, preds, labels):
         pic_fn = os.path.join(viz_folder,
                               '{}{}{}.png'.format(prefix, fn, suffix))
-        plot_yhat(pred, label, fn=pic_fn)
+        plot_yhat(pred, label, fn=pic_fn, threshold=threshold)
 
 
-def plot_yhat(y_hat, label, fn=None):
+def plot_yhat(y_hat, label, fn=None, threshold=None):
     fig = plt.figure()
     ax = fig.gca()
     ax.plot(y_hat[:, 1], label='Prediction')
     ax.plot(label, label='Ground Truth')
-    ax.legend()
+    if threshold:
+        plt.axhline(threshold, 0, 1, linestyle='--',
+                    color='k', label='Threshold')
+        classifications = np.asarray(y_hat[:, 1] >= threshold, dtype=int)
+        plt.fill_between(np.arange(len(classifications)),
+                         y_hat[:, 1] * classifications, 0,
+                         color='blue',       # The outline color
+                         alpha=0.2)
+
+    ax.set_xlabel('Time (s)', fontsize=16)
+    ax.set_ylabel('Model Output', fontsize=16)
+    ax.tick_params(axis='x', labelsize=16)
+    ax.tick_params(axis='y', labelsize=16)
+    # ax.legend(ncol=3, loc='upper right')
+    # ax.set_ylim([0, 10])
+    ax.set_xlim([0, len(y_hat[:, 1])])
     if fn:
+        plt.tight_layout()
         plt.savefig(fn)
         plt.close()
+
     else:
         plt.show()
 
